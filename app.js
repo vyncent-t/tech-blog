827e2d234873fb171148a3b315942503c302f408
Vincent T <vyncode@gmail.com>
2021-07-14 18:32:14 -0400

    working on fixing missing styles

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ const routes = require("./controllers");
 const app = express();
 const PORT = process.env.DB_PORT || 3000;
 
-app.engine("handlebars", exphbs());
+app.engine("handlebars", exphbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main'
+}));
 app.set("view engine", "handlebars");
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
