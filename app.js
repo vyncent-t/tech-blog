24bfb9f3e4e8962388f2b96798f79ae0f6965fe1
Vincent T <vyncode@gmail.com>
2021-07-24 14:06:39 -0400

    fixing app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const routes = require("./controllers");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const sess = {
     secret: 'secret',
